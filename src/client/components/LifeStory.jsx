6dae4aa830a893eaec2ad92d3af6f41f7846565c
Han Xiao <yhxiao97@gmail.com>
2019-07-21 10:07:30 -0400

    updated life story

--- src/client/components/LifeStory.jsx
+++ src/client/components/LifeStory.jsx
@@ -23,25 +23,31 @@ class LifeStory extends Component {
                     <Col md={8}>
 
                         <p>
-                            I was afraid of heights and one day
-                            when I was 15, I decided I wanted to conquer my fear and fly. By age 17 I obtained both my glider and private pilot
-                            licenses. To say that it happened without a hitch and without tears, sweat and anxiety would be a lie.
-                            One thing I learned from becoming a pilot is that, there are no such things as "free lunches" in life. Everything requires
-                            sacrifice and effort, and, of course, a bit of luck.
+                        I was afraid of heights growing up, then I decided  that I wanted to conquer this 
+                        fear by flying a plane. By age 17, I obtained both my glider and private pilot licenses. 
+                        To say that it happened without a hitch and without tears, sweat and anxiety would be a 
+                        big lie. One thing I learned from becoming a pilot is that there are no such things as 
+                        "free lunches" in life. Everything requires sacrifice and effort, and, of course, 
+                        a bit of luck.
+
                         </p>
 
                         <p>
-                            When I was an edgy teenager at 18, I co-founded a non-for-profit organization that raises awareness for a heart disease affecting children.
-                            Through that roller-coaster experience, I learned that making an impact is easier said than done. And I learned that lesson
-                            through hard work, paperworks, heated discussions and compromises.
+                            When I was 18, I co-founded a non-for-profit organization that raises awareness for a heart disease affecting children.
+                            Through that roller-coaster experience, I learned that making an impact is easier said than done. And I learned that 
+                            after long nights of hard work, paperworks, heated discussions and compromises.
                         </p>
 
                         <p>
-                            As I moved on to the next chapter of my life, I now aspire to become a software engineer, I believe that my love for problem solving
-                            and grit will help me make a positive impact in technology.
-                            I currently study at the University of Waterloo in Canada 🇨🇦 in software engineering with a minor in computational mathematics.
-
-                            <br />
+                        As I moved on to the next chapter of my life, 
+                        I now aspire to become a software engineer and currently study at the 
+                        University of Waterloo 🇨🇦. 
+                        I believe that my grit and relentlessness facing hard problems 
+                        will help me make a positive impact using technology. 
+                        Using the tools and experiences learned from my past experiences, 
+                        I am ready to put in the necessary effort and passion into conquering 
+                        whatever next that comes through my door. 
+                        <br />
 
 
                         </p>
