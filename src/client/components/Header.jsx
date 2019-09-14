a2677122e7fabb8cc89f193c3d3920e21e7cb6d7
Han Xiao <yhxiao97@gmail.com>
2019-09-14 14:55:14 -0400

    Modifying website

--- src/client/components/Header.jsx
+++ src/client/components/Header.jsx
@@ -6,6 +6,9 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faLinkedin, faFacebook, faGithub, faMedium } from '@fortawesome/free-brands-svg-icons'
 import { faEnvelope } from '@fortawesome/fontawesome-free-solid'
 
+import posed from 'react-pose';
+
+
 class Header extends Component {
     constructor(props) {
         super(props);
@@ -16,50 +19,67 @@ class Header extends Component {
         this.setState({ collapse: !this.state.collapse });
     }
 
+    getContactMethods() {
+        return (
+            <Collapse open={this.state.collapse}>
+                <div className="p-3 mt-2 rounded">
+                    <span>Here are the ways you can reach out to me</span>
+
+                    <a class="twitter mx-3 h4 d-inline-block text-secondary" href="https://www.linkedin.com/in/hanburger/" target="_blank">
+                        <FontAwesomeIcon icon={faLinkedin} color='grey' />
+                    </a>
+                    <a class="twitter mx-3 h4 d-inline-block text-secondary" href="mailto:yhxiao@uwaterloo.ca" target="_blank">
+                        <FontAwesomeIcon icon={faEnvelope} color='grey' />
+
+                    </a>
+                    <a class="github mx-3 h4 d-inline-block text-secondary" href="https://github.com/hanburger97" target="_blank">
+                        <FontAwesomeIcon icon={faGithub} color='grey' />
+
+                    </a>
+
+                    <a class="facebook mx-3 h4 d-inline-block text-secondary" href="https://www.facebook.com/hanburger97" target="_blank">
+                        <FontAwesomeIcon icon={faFacebook} color='grey' />
+
+                    </a>
+                    <a class="twitter mx-3 h4 d-inline-block text-secondary" href="https://medium.com/@hanburger97" target="_blank">
+                        <FontAwesomeIcon icon={faMedium} color='grey' />
+
+                    </a>
+
+                </div>
+            </Collapse>
+        )
+    }
+
+    getAnimatedShard() {
+        
+    }
+
     render() {
+
         return (
             <div className={HeaderStyle.header}>
                 <div className={HeaderStyle.headerContent}>
                     <Row>
-                        <Col md={7} >
-                            <h1>Han Xiao</h1>
+                        <Col md={5} >
+                        <h1>Han Xiao</h1>
+
                             <br />
-                            Aspriring software engineer & week-end pilot
+                            <p>
+                                Aspriring software engineer interested in cloud development and distributed computing.
+                                Aviation enthusiast and cessna pilot.
+                            </p>  
+                            
                             <br />
-                            <br />
-                            {/* <Button pill href="https://drive.google.com/file/d/1CsGcQbhzSFI9opxw30pe6dJtCM3wvoKc/view?usp=sharing" > Resume </Button> */}
+                            <Button pill href="https://github.com/hanburger97">
+                                Github
+                            </Button>
                             <Button pill outline onClick={this.toggle}>
                                 Get in touch
                             </Button>
-                            <Collapse open={this.state.collapse}>
-                                <div className="p-3 mt-2 rounded">
-                                    <span>Here are the ways you can reach out to me</span>
-
-                                    <a class="twitter mx-3 h4 d-inline-block text-secondary" href="https://www.linkedin.com/in/hanburger/" target="_blank">
-                                        <FontAwesomeIcon icon={faLinkedin} color='grey' />
-                                    </a>
-                                    <a class="twitter mx-3 h4 d-inline-block text-secondary" href="mailto:yhxiao@uwaterloo.ca" target="_blank">
-                                        <FontAwesomeIcon icon={faEnvelope} color='grey' />
-
-                                    </a>
-                                    <a class="github mx-3 h4 d-inline-block text-secondary" href="https://github.com/hanburger97" target="_blank">
-                                        <FontAwesomeIcon icon={faGithub} color='grey' />
-
-                                    </a>
-
-                                    <a class="facebook mx-3 h4 d-inline-block text-secondary" href="https://www.facebook.com/hanburger97" target="_blank">
-                                        <FontAwesomeIcon icon={faFacebook} color='grey' />
-
-                                    </a>
-                                    <a class="twitter mx-3 h4 d-inline-block text-secondary" href="https://medium.com/@hanburger97" target="_blank">
-                                        <FontAwesomeIcon icon={faMedium} color='grey' />
-
-                                    </a>
-
-                                </div>
-                            </Collapse>
+                            {this.getContactMethods()}
                         </Col>
-                        <Col md={4}>
+                        <Col md={7}>
 
                         </Col>
                     </Row>
