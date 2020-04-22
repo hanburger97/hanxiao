ac1feba2e0967080e3d2b1a8f0ad3d23edaac793
Han Xiao <yhxiao97@gmail.com>
2020-04-22 12:53:04 -0400

    new version

--- src/client/components/Header.jsx
+++ src/client/components/Header.jsx
@@ -57,24 +57,27 @@ class Header extends Component {
                         <br/>
                         <p>
                             Third-year software engineering student at the <Link href="https://uwaterloo.ca/" target="_blank">University of Waterloo, </Link>
-                            passionate about distributed systems, SRE and databases and  
+                            passionate about distributed systems, SRE and databases. I am  
                             looking for impactful internships in fast growing companies.
                         </p> 
                         <p>
-                            Previous experience includes production engineering and back-end at <Link href="https://www.shopify.ca/" target="_blank">Shopify</Link>, cloud back-end at <Link href="https://www.bynorth.com/" target="_blank">North </Link>(Thalmic Labs) and data engineering at Intact Insurance's data lab.
+                            Previous experiences includes production engineering and back-end development at <Link href="https://www.shopify.ca/" target="_blank">Shopify</Link>, cloud development at <Link href="https://www.bynorth.com/" target="_blank">North </Link>(Thalmic Labs) and data engineering at Intact Insurance's data lab.
                         </p>
                         <p>
-                            I am a pilot and aviation lover. I am always seeking new adventures in travelling, in meeting new people and in trying new experiences. In my free time you can find
-                            me <Link href="https://www.goodreads.com/user/show/113736962-han-xiao" target="_blank">reading </Link> and  <Link href="https://medium.com/@hanx.ca" target="_blank">writing</Link>.
+                            I am a pilot and an aviation lover. I am always seeking new adventures and thrills in travelling around the world, in meeting new people and in trying new experiences. In my free time, I love to 
+                             <Link href="https://www.goodreads.com/user/show/113736962-han-xiao" target="_blank"> read </Link> and  <Link href="https://medium.com/@hanx.ca" target="_blank">write.</Link> I also enjoy snowboarding, volleyball and rugby. 
                         </p>
                         {this.getContactMethods()}
                         
                         
                     </div>
                 </Col>
-                <Col md={5} sm={12}>
+                <Col md={6} sm={12}>
 
-                    <img src="public/img/world.svg" style={{height: "20rem", width: "auto", marginTop: "10rem"}}/>
+                    <img src="https://res.cloudinary.com/dhrmym6ox/image/upload/v1580508229/IMG_0362.jpg" style={{height: "15rem", width: "auto", left: "5rem", top: "13rem", position:"absolute", zIndex: 3, boxShadow: "10px 10px 5px rgba(0, 0, 0, 0.4)"}} />
+                    <img src="https://res.cloudinary.com/dhrmym6ox/image/upload/v1587573769/italy.jpg" style={{height: "10rem", width: "auto", left: "12rem", top: "9rem", position:"absolute", zIndex: 1, boxShadow: "10px 10px 5px rgba(0, 0, 0, 0.4)"}} />
+                    <img src="https://res.cloudinary.com/dhrmym6ox/image/upload/v1587573344/IMG_0642.jpg" style={{height: "17rem", width: "auto", left: "16rem", top: "16rem", position:"absolute", zIndex: 2, boxShadow: "10px 10px 5px rgba(0, 0, 0, 0.4)"}} />
+                    <img src="public/img/world.svg" style={{height: "30rem", width: "auto", zIndex: 0, left: "0rem", top: "4rem",position: "absolute"}}/>
                 </Col>
             </Row>     
                    
