647025d6817d9516a7a50255a8e0880f3d4f2c08
Han Xiao <yhxiao97@gmail.com>
2020-04-27 20:55:02 -0400

    Adding exercise tracker ;

--- src/client/App.jsx
+++ src/client/App.jsx
@@ -4,6 +4,15 @@ import { withRouter, Route, NavLink, Switch } from 'react-router-dom';
 import HomePage from './pages/Home.jsx';
 import MotivationPage from './pages/MotivationPage.jsx';
 
+const ExperciseLog = (props) => {
+    return <>
+        <script src="https://static.airtable.com/js/embed/embed_snippet_v1.js"></script>
+        <iframe className="airtable-embed airtable-dynamic-height" src="https://airtable.com/embed/shrz0KF2FKQ1Qgah6?backgroundColor=gray" 
+        frameBorder="0" onMouseWheel="" width="100%" height="800rem" style={{background: "transparent", border: "1px solid #ccc"}}/>
+    </>
+};
+
+
 
 class App extends Component {
     render() {
@@ -11,6 +20,7 @@ class App extends Component {
             <div>
                 <Switch>
                     <Route path='/motivation' component={MotivationPage} />
+                    <Route path="/log" component={ExperciseLog}/> 
                     <Route path='/' component={HomePage} />
                 </Switch>
             </div>
